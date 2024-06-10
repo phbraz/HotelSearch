9bac1ebdb7a0ebcc0fad10b4983f02d40a091eae
Paulo Braz <ph_braz@hotmail.com>
2024-06-10 17:06:41 +0100

    Added a controller to handle our requestes, created a generic method to handle any kind of request for mediatR.
    With that generic handler it's easier to scale the application if we want to.

---
+++ HotelSearchAPI/Controllers/HolidaySearchController.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTO;
+using Application.Queries.HolidaySearch;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelSearchAPI.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("HolidaySearch")]
+public class HolidaySearchController(IMediator mediator) : MainBase(mediator)
+{
+    [HttpPost("FetchHolidayDeals")]
+    public async Task<HolidaySearchResponse> FetchHolidayDeals([FromBody] GetHolidayDealQuery query, CancellationToken token)
+        => await SendQuery<GetHolidayDealQuery, HolidaySearchResponse>(query, token);
+}
---
+++ HotelSearchAPI/Controllers/MainBase.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelSearchAPI.Controllers;
+
+public class MainBase(IMediator mediator) : ControllerBase
+{
+    protected async Task<TResponse> SendQuery<TRequest, TResponse>(TRequest query, CancellationToken cancellationToken)
+        where TRequest : IRequest<TResponse>
+    {
+        return await mediator.Send(query, cancellationToken);
+    }
+}
