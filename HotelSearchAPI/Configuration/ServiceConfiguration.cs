cbd80d842c2c21fad2200d58643e56968c662098
Paulo Braz <ph_braz@hotmail.com>
2024-06-10 15:20:14 +0100

    Added MediatR and some configuration.

---
+++ HotelSearchAPI/Configuration/ServiceConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelSearchAPI.Configuration;
+
+public static class ServiceConfiguration
+{
+    public static void ConfigurationService(IServiceCollection services)
+    {
+        //add our services 
+    }
+}
--- HotelSearchAPI/Program.cs
+++ HotelSearchAPI/Program.cs
@@ -1,9 +1,21 @@
+using System.Net.Mime;
+using HotelSearchAPI.Configuration;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+ServiceConfiguration.ConfigurationService(builder.Services);
+builder.Services.AddControllers();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(MediaTypeNames.Application).Assembly);
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
@@ -15,4 +27,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 app.Run();
